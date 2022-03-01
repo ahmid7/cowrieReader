00b99e7cd2dce266ee6eb8704d44b9484dc2c7f1
Sanni Odunayo <ahmidsanni@gmail.com>
2022-03-01 13:16:02 +0100

    Started adding design for start button

--- src/Pages/HeroPage.jsx
+++ src/Pages/HeroPage.jsx
@@ -1,8 +1,18 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 const HeroPage = () => {
   let navigate = useNavigate()
-
+  const [ buttonClick, setButtonClick ] = useState(false)
+  
+  
+  function click() {
+    setTimeOut(() =>{
+      navigate('consult')
+    },3000)
+    setButtonClick(true)
+  }
+  
   return (
     <div className="w-screen h-screen flex-center bg-dark-blue">
         <div className="text-[#FFFFFF] flex-center flex-col">
@@ -17,7 +27,7 @@ const HeroPage = () => {
                 <h2 className="font-bold text-base lg:text-lg xl:text-2xl">Cowrie Reader</h2>
             </div>
 
-            <div className=" w-20 h-20 xl:w-24 xl:h-24 design-border-outer-green flex-center rounded-full mt-12 mouse-hover outline-none focused:outline-none" onClick={() => {navigate('consult')}}>
+            <div className=" w-20 h-20 xl:w-24 xl:h-24 design-border-outer-green flex-center rounded-full mt-12 mouse-hover outline-none focused:outline-none" onClick={() => {click()}}>
                 <div className="bg-dark-blue w-[75px] h-[75px] xl:w-[85px] xl:h-[85px] flex-center rounded-full ">
                     <div className="w-14 h-14 xl:w-16 xl:h-16 design-border-inner-green rounded-full flex-center font-semibold text-sm text-green mouse-hover outline-none">
                         <div className="bg-dark-blue flex-center rounded-full w-[54px] h-[54px] xl:w-[60px] xl:h-[60px]">
